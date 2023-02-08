231dd331140f9742d284f4669f12cc28d58ff006
emanuelefavero <emafaux89@gmail.com>
2023-02-09 00:24:55 +0100

    add minimal trpc configuration

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,3 +1,27 @@
-console.log('hi')
+// * IMPORT TRPC CLIENT
+// npm i @trpc/client
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
+// * IMPORT TYPES we created in server/api.ts
+import { AppRouter } from '../../server/api'
 
-export {}
+// * Create a client
+const client = createTRPCProxyClient<AppRouter>({
+  // httpBatchLink batches multiple requests into one - useful for performance
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
+})
+
+// * Make requests to the server
+async function main() {
+  // http://localhost:3000/trpc/sayHi
+  const result = await client.sayHi.query()
+  console.log(result)
+
+  // http://localhost:3000/trpc/logToServer
+  await client.logToServer.mutate('hello from client') // check server console
+}
+
+main()
--- server/api.ts
+++ server/api.ts
@@ -1,7 +1,42 @@
 import express from 'express'
 import cors from 'cors'
 
+// * IMPORT TRPC SERVER
+// npm i @trpc/server
+import { initTRPC } from '@trpc/server'
+import { createExpressMiddleware } from '@trpc/server/adapters/express'
+
+// * Initialize TRPC
+const t = initTRPC.create()
+
+// * Define the router and its procedures (endpoints)
+const appRouter = t.router({
+  // http://localhost:3000/trpc/sayHi
+  sayHi: t.procedure.query(() => {
+    return 'Hi from trpc server'
+  }),
+  // http://localhost:3000/trpc/logToServer
+  // http://localhost:3000/trpc/logToServer?input=hello
+  logToServer: t.procedure
+    .input((v) => {
+      if (typeof v === 'string') return v
+
+      throw new Error('Invalid input: expected string')
+    })
+    .mutation((req) => {
+      console.log(`Client said: ${req.input}`)
+      return true
+    }),
+})
+
 const app = express()
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ origin: 'http://localhost:5173' })) // cors for client
+
+// * Add the TRPC middleware to express (express specific)
+// http://localhost:3000/trpc
+app.use('/trpc', createExpressMiddleware({ router: appRouter }))
 
 app.listen(3000, () => console.log('Server started on port 3000'))
+
+// * export types for client
+export type AppRouter = typeof appRouter
